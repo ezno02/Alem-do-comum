98c4ce49ad0286d04ce728f2dc1dac0a53e30076
ezno02 <enzo.bmachado.e@gmail.com>
2025-05-24 18:12:44 -0300

    commit05 24-05-2025 (now_more and scroll-horizontal)

--- scroll-horizontal.js
+++ scroll-horizontal.js
@@ -5,3 +5,19 @@ window.scrollBy({
     behavior: 'smooth'
     });
 }, { passive: false });
+
+function voltar(){
+    let pagVoltarAction = document.getElementById("pag1")
+    let divAction = document.getElementById("quadro01")
+    let sombraAction = document.getElementById("sombraQuadro01")
+
+    pagVoltarAction.style.backgroundColor = "white"
+    setTimeout(() => {
+        pagVoltarAction.style.backgroundColor = "black"
+        divAction.style.backgroundColor = "black"
+        sombraAction.style.backgroundColor = "black"
+        setTimeout(() => {
+            open("index.html", "_self")
+        }, 900);
+    }, 800);
+}
